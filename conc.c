45de208a990c9e00ea69e39b17e0aef3e990d22b
ewn2 <erwin.uppal@gmail.com>
2019-05-19 00:52:22 -0700

    Fixed customer eating 101 items

--- conc.c
+++ conc.c
@@ -12,6 +12,133 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <stdbool.h>
 #include <signal.h>
 #include <time.h>
+#include <pthread.h>
+#include <semaphore.h>
 
+pthread_mutex_t Padlock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t ChefSem = PTHREAD_COND_INITIALIZER;
+pthread_cond_t Ender = PTHREAD_COND_INITIALIZER;
+pthread_cond_t Fries = PTHREAD_COND_INITIALIZER;
+pthread_cond_t Hamburger = PTHREAD_COND_INITIALIZER;
+pthread_cond_t Soda = PTHREAD_COND_INITIALIZER;
+
+int ChefMake = 0;
+int Servings = 0;
+int Cust1Scarfs = 0;
+int Cust2Scarfs = 0;
+int Cust3Scarfs = 0;
+int Customer = 0;
+
+void *Chef(){
+    Customer = 0;
+    while(Servings <= 100) {
+        Servings++;
+        pthread_mutex_lock(&Padlock);
+        while(ChefMake != 0) {
+            pthread_cond_wait(&ChefSem, &Padlock);
+        }
+        ChefMake = ((rand() % (3)) + 1);
+        if(ChefMake == 3 && Servings <= 100) {
+            printf("Chef Produces Fries and Hamburger and falls asleep\n");
+            pthread_cond_signal(&Soda);
+            sleep(1);
+        }
+        if(ChefMake == 1 && Servings <= 100) {
+            printf("Chef Produces Fries and Soda and falls asleep\n");
+            pthread_cond_signal(&Hamburger);
+            sleep(1);
+        }
+        if(ChefMake == 2 && Servings <= 100) {
+            printf("Chef Produces Soda and Hamburger and falls asleep\n");
+            pthread_cond_signal(&Fries);
+            sleep(1);
+        }
+        pthread_mutex_unlock(&Padlock);
+    }
+    pthread_cond_signal(&Ender);
+}
+
+void *Finish(){
+    pthread_mutex_lock(&Padlock);
+    pthread_cond_wait(&Ender, &Padlock);
+    printf("\nKitchen Closed\n\n");
+    printf("Customer 1 ate %d times\n", Cust1Scarfs);
+    printf("Customer 2 ate %d times\n", Cust2Scarfs);
+    printf("Customer 3 ate %d times\n", Cust3Scarfs);
+    if(Cust1Scarfs >= 40) {
+        printf("Customer 1 is a greedy little thing, ain't he?\n");
+    }
+    if(Cust2Scarfs >= 40) {
+        printf("Customer 2 is a greedy little thing, ain't he?\n");
+    }
+    if(Cust3Scarfs >= 40) {
+        printf("Customer 3 is a greedy little thing, ain't he?\n");
+    }
+    exit(0);
+}
+
+void *Customer1(){
+    while(Servings <= 100){
+        pthread_mutex_lock(&Padlock);
+        while(ChefMake != 1) {
+            pthread_cond_wait(&Hamburger, &Padlock);
+        }
+        Customer = 1;
+        printf("Customer #%d gets Fries and Soda, combines with their own Hamburger\n", Customer);
+        pthread_cond_signal(&ChefSem);
+        ChefMake = 0;
+        pthread_mutex_unlock(&Padlock);
+        printf("Customer #%d consumes full meal and rings bell\n", Customer);
+        Cust1Scarfs++;
+    }
+}
+
+void *Customer2() {
+    while(Servings <= 100) {
+        pthread_mutex_lock(&Padlock);
+        while(ChefMake != 2) {
+            pthread_cond_wait(&Fries, &Padlock);
+        }
+        Customer = 2;
+        printf("Customer #%d gets Soda and Hamburger, combines with their own Fries\n", Customer);
+        pthread_cond_signal(&ChefSem);
+        ChefMake = 0;
+        pthread_mutex_unlock(&Padlock);
+        printf("Customer #%d consumes full meal and rings bell\n", Customer);
+        Cust2Scarfs++;
+    }
+}
+
+void *Customer3(){
+    while(Servings <= 100){
+        pthread_mutex_lock(&Padlock);
+        while(ChefMake != 3) {
+            pthread_cond_wait(&Soda, &Padlock);
+        }
+        Customer = 3;
+        printf("Customer #%d gets Fries and Hamburger, combines with their own Soda\n", Customer);
+        pthread_cond_signal(&ChefSem);
+        ChefMake = 0;
+        pthread_mutex_unlock(&Padlock);
+        printf("Customer #%d eats full meal and rings bell\n", Customer);
+        Cust3Scarfs++;
+    }
+}
+
+int main() {
+    pthread_t ChefThread, EnderThread, Customer1Thread, Customer2Thread, Customer3Thread;
+    srand(time(NULL));
+    pthread_create(&ChefThread,NULL,Chef,NULL);
+    pthread_create(&EnderThread,NULL,Finish,NULL);
+    pthread_create(&Customer1Thread,NULL,Customer1,NULL);
+    pthread_create(&Customer2Thread,NULL,Customer2,NULL);
+    pthread_create(&Customer3Thread,NULL,Customer3,NULL);
+    pthread_join(ChefThread, NULL);
+    pthread_join(EnderThread, NULL);
+    pthread_join(Customer1Thread, NULL);
+    pthread_join(Customer2Thread, NULL);
+    pthread_join(Customer3Thread, NULL);
+}
